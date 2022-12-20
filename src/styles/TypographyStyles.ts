fda054d36543866a033309c5f1c0ee4d8c25a4dc
Flemming Eriksson <flemming.eriksson@consid.se>
2022-12-20 23:57:28 +0100

    Typography styling

---
+++ src/styles/TypographyStyles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  h1: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  h2: {
+    fontSize: 16,
+    marginBottom: 48,
+  },
+});
